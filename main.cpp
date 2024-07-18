126b2bcab067932e452ea790401d0eb50c3785f8
Irish Rivera <162866325+kelleeerrrr@users.noreply.github.com>
2024-07-18 22:13:39 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <limits>
+#include <algorithm>
+#include <chrono>
+#include <ctime>
 
+using namespace std;
+
+#define ADD_QUESTION 1
+#define EDIT_QUESTION 2
+#define DELETE_QUESTION 3
+#define VIEW_ALL_QUESTIONS 4
+#define VIEW_RESULTS_BY_USER 5
+#define VIEW_RESULTS_BY_QUIZ 6
+#define GENERATE_REPORT 7
+#define ADMIN_LOGOUT 8
+
+#define TAKE_QUIZ 1
+#define VIEW_SCORES 2
+#define RESET_PASSWORD 3
+#define USER_LOGOUT 4
+
+#define SIGNUP_ADMIN 1
+#define SIGNUP_USER 2
+#define LOGIN_ADMIN 3
+#define LOGIN_USER 4
+#define EXIT 5
